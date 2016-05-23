70ce6777add366d452043a1c5cd345eec26b73fe
Johanna Choez <patuchita.preciosa@gmail.com>
2016-05-23 09:17:31 -0500

    TareaPersona

---
+++ tarea_Persona/src/tarea_Persona/persona.java
@@ -0,0 +1,15 @@
+package tarea_Persona;
+
+public class persona {
+	
+	public persona(){
+		
+	}
+    public String cedulaIdentidad;
+    public String nombre;
+    public String apellido;
+    public int fechaNacimiento;
+    public String telefono;
+    public String lugarResidencia;
+    public int casado;
+}
