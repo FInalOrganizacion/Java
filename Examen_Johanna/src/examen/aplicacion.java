015fa50c12cea42a15b3f8128e9c62b9cccb99f5
Johanna Choez <patuchita.preciosa@gmail.com>
2016-05-30 10:08:24 -0500

    Ejercicio5

---
+++ Examen_Johanna/src/examen/aplicacion.java
@@ -0,0 +1,38 @@
+package examen;
+import javax.swing.JOptionPane;
+
+
+public class aplicacion {
+ private String cad;
+  String cad2,cad3; 
+public String getcad(){
+	return cad;
+}
+public void setcad(){
+	cad=cad;
+}
+ /*public  aplicacion(String cad){
+	 this.cad=cad;  
+ }
+ */
+ public void mayuscula(){
+	 int aux = cad.length(); 
+	 
+	 if(aux < 3){
+		 cad.toUpperCase(); 
+	 }else{
+		 
+		 
+		 cad2 = cad.substring(0,cad.length()-3); 
+		 cad3 = cad.substring(cad.length()-3); 
+		 JOptionPane.showMessageDialog(null, cad2+cad3.toUpperCase());
+	 }
+ }
+ 
+
+ 
+ protected void finalize(){
+	 JOptionPane.showMessageDialog(null, "CERRANDO PROGRAMA", "ATENCION", JOptionPane.WARNING_MESSAGE);
+}
+
+}
---
+++ Examen_Johanna/src/examen/main.java
@@ -0,0 +1,18 @@
+package examen;
+
+
+import javax.swing.JOptionPane;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		String cad; 
+		cad.setcad(JOptionPane.showInputDialog("Ingrese una palabra:"));
+
+		JOptionPane.showInputDialog( null, "Palabra",cad) ;
+		
+	    System.gc();
+	}
+	
+}
